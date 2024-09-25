d90506a15e92ae6968296c7872b4918bba83f261
Pavan sai <pavanaws57@gmail.com>
2024-09-25 11:48:22 +0530

    updates

---
+++ eks-terraform/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.67.0"
+    }
+  }
+  backend "s3" {
+    bucket = "eks-buckets"
+    key = "eks/terraform.tfstate"
+    region = "us-east-1"
+  }
+  required_version = ">= 1.6.3"
+}
---
+++ eks-terraform/main.tf
@@ -0,0 +1,181 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+#Create IAM Role for EKS Cluster
+resource "aws_iam_role" "master" {
+  name = "pavan-eks-master"
+  assume_role_policy = jsondecode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "eks.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+#Attach IAM Role Policies for EKS Cluster
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+   role = aws_iam_role.master.name
+   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy" 
+  
+}
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+  role = aws_iam_role.master.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
+resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
+  role = aws_iam_role.master.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+#Create IAM Role for Worker Node
+resource "aws_iam_role" "worker" {
+  name = "pavan-eks-worker"
+  assume_role_policy = jsondecode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+#Create IAM Policy for Autoscaler
+resource "aws_iam_policy" "autoscaler" {
+    name = "pavan-eks-autoscaler-policy"
+    policy = jsondecode({
+        "Version" : "2012-10-17",
+        "Statement" : [
+          {
+            "Action" : [
+                "autoscaling:DescribeAutoScalingGroups",
+                "autoscaling:DescribeAutoScalingInstances",
+                "autoscaling:DescribeTags",
+                "autoscaling:DescribeLaunchConfigurations",
+                "autoscaling:SetDesiredCapacity",
+                "autoscaling:TerminateInstanceInAutoScalingGroup",
+                "ec2:DescribeLaunchTemplateVersions"
+            ],
+            "Effect"   : "Allow",
+            "Resource" : "*"
+          }
+        ]
+    })
+}
+
+#Attach IAM Role Policies for worker Nodes
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  role = aws_iam_role.worker.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  role = aws_iam_role.worker.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+resource "aws_iam_role_policy_attachment" "AmazonSSMManagedInstanceCore" {
+  role = aws_iam_role.worker.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  role = aws_iam_role.worker.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+resource "aws_iam_role_policy_attachment" "s3" {
+  role = aws_iam_role.worker.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+resource "aws_iam_role_policy_attachment" "autoscaler" {
+  role = aws_iam_role.worker.name
+  policy_arn = aws_iam_policy.autoscaler.arn
+}
+#Attach IAM Role to worker Node Profile
+resource "aws_iam_instance_profile" "worker" {
+  depends_on = [ aws_iam_role.worker ]
+  name = "pavan-eks-worker-new-profile"
+  role = aws_iam_role.worker.name
+}
+
+#Data Sources for VPC, Subnets, and Security Group
+data "aws_vpc" "main" {
+  tags = {
+    Name = "py_vpc"
+  }
+}
+data "aws_subnet" "subnet-1" {
+  vpc_id = data.aws_vpc.main.id
+  filter {
+    name = "tag:Name"
+    values = [ "py-pub-sub-1" ]
+  }
+}
+data "aws_subnet" "subnet-2" {
+  vpc_id = data.aws_vpc.main.id
+  filter {
+    name = "tag:Name"
+    values = [ "py-pub-sub-2" ]
+  }
+}
+data "aws_security_group" "selected" {
+  vpc_id = data.aws_vpc.main.id
+  filter {
+    name = "tag:Name"
+    values = ["py-sg"]
+  }
+}
+#Create EKS Cluster
+resource "aws_eks_cluster" "eks" {
+    name = "project-eks"
+    role_arn = aws_iam_role.master.arn
+    vpc_config {
+      subnet_ids = [data.aws_subnet.subnet-1, data.aws_subnet.subnet-2]
+    }
+    tags = {
+        Name = "MY_EKS"
+    }
+    depends_on = [ 
+        aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+        aws_iam_role_policy_attachment.AmazonEKSServicePolicy,
+        aws_iam_role_policy_attachment.AmazonEKSVPCResourceController
+    ]
+  
+}
+#Create Node Group for EKS Cluster
+resource "aws_eks_node_group" "node-grp" {
+    cluster_name = aws_eks_cluster.eks.name
+    node_group_name = "project-group-name"
+    node_role_arn = aws_iam_role.worker.arn
+    subnet_ids = [ data.aws_subnet.subnet-1, data.aws_subnet.subnet-2 ]
+    capacity_type = "ON_DEMAND"
+    disk_size = 20
+    instance_types = ["t2.medium"]
+    remote_access {
+      ec2_ssh_key = "Practice"
+      source_security_group_ids = [ data.aws_security_group.selected.id ]
+    }
+    labels = {
+      env = "dev"
+    }
+    scaling_config {
+      desired_size = 2
+      max_size = 4
+      min_size = 1
+    }
+    update_config {
+      max_unavailable = 1
+    }
+    depends_on = [ 
+        aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+        aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+        aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly
+    ]
+}
